eab201b4a4a053a752457d36c5dbbed474f6425e
Camile Lendering <camile.lendering@gmail.com>
2023-12-01 21:01:24 +0100

    Incorporated dialogue history, slot-value parser, also  added function for full evaluation of performance on test data.

--- slot-filling-reformatted.py
+++ slot-filling-reformatted.py
@@ -1,11 +1,13 @@
 from datasets import load_dataset
 import torch
 import torch.nn as nn
-from transformers import BertTokenizerFast, BertForTokenClassification, BertConfig
-from torch.utils.data import DataLoader, Dataset
+from transformers import BertTokenizerFast, BertForTokenClassification
+from torch.utils.data import Dataset
 from seqeval.metrics import classification_report as seqeval_classification_report
 import numpy as np
+import pandas as pd
 from tqdm.auto import tqdm
+import typing
 
 
 class SlotFillingDataset:
@@ -241,26 +243,25 @@ class SlotFillingModel:
         id2label,
         max_length=128,
         batch_size=32,
-        epochs=3,
+        epochs=4,
         patience=2,
         lr=2e-5,
     ):
         """
-        Initializes the SlotFilling class.
-        :param dataset: SlotFillingDataset object.
-        :param model: Model to use for training.
+        Initializes the SlotFillingModel class.
+        :param train_dataloader: Dataloader for training.
+        :param val_dataloader: Dataloader for validation.
+        :param test_dataloader: Dataloader for testing.
         :param tokenizer: Tokenizer object used for tokenizing texts.
+        :param num_labels: Number of unique labels.
+        :param label2id: Mapping from labels to ids.
+        :param id2label: Mapping from ids to labels.
         :param max_length: Maximum length of the tokenized inputs.
         :param batch_size: Batch size for training.
         :param epochs: Number of epochs for training.
-        :param lr: Learning rate for training.
-        :param warmup_steps: Warmup steps for training.
-        :param patience: Patience for early stopping.
-        :param num_labels: Number of labels.
-        :param label2id: Mapping from labels to ids.
-        :param id2label: Mapping from ids to labels.
+        :param patience: Number of epochs to wait for improvement before early stopping.
+        :param lr: Learning rate for the optimizer.
         """
-        self.dataset = dataset
         self.model = None
         self.tokenizer = tokenizer
         self.max_length = max_length
@@ -478,7 +479,7 @@ class SlotFillingModel:
             last_word_end = word_end
 
         return word_labels
-    
+
     def query_slots(self, utterance):
         if not self.model:
             print("Model not created.")
@@ -516,14 +517,16 @@ class SlotFillingModel:
         current_slot = None
         current_value = ""
 
-        for token, label_id, (word_start, word_end) in zip(tokens, predictions, offset_mapping):
+        for token, label_id, (word_start, word_end) in zip(
+            tokens, predictions, offset_mapping
+        ):
             label = self.id2label.get(label_id, "O")
 
             if label.startswith("B-"):
                 # Save the previous slot and value if any
                 if current_slot:
                     slot_values[current_slot] = current_value.strip()
-                
+
                 current_slot = label[2:]  # Remove the 'B-' prefix
                 current_value = utterance[word_start:word_end]
 
@@ -543,7 +546,166 @@ class SlotFillingModel:
 
         return slot_values
 
-    
+
+class DialogSlotMemory:
+    slot_list_dict: typing.Dict[str, typing.List[str]] = {}
+
+    def __init__(self):
+        self.slot_list_dict = {}
+
+    def add_slot(self, slot_name: str, slot_value: str):
+        if slot_name not in self.slot_list_dict:
+            self.slot_list_dict[slot_name] = []
+        self.slot_list_dict[slot_name].append(slot_value)
+
+    def get_slot_values(self, slot_name: str):
+        return self.slot_list_dict[slot_name]
+
+    def get_most_recent_slot_value(self, slot_name: str):
+        return (
+            self.slot_list_dict[slot_name][-1]
+            if slot_name in self.slot_list_dict
+            else None
+        )
+
+    def get_all_slot_values(self):
+        return self.slot_list_dict
+
+    def get_all_slot_names(self):
+        return self.slot_list_dict.keys()
+
+
+class SlotValueParser:
+    def extract_slot_spans(self, dataset):
+        """
+        Extracts unique tuples of actual slot values and their spans in the utterance.
+        :param dataset: Dataset to extract slot spans from.
+        """
+        extracted_data = []
+
+        for dialogue in dataset:
+            turns = dialogue["turns"]
+            for i, _ in enumerate(turns["turn_id"]):
+                utterance = turns["utterance"][i]
+
+                # Extracting slot values and their spans
+                if "dialogue_acts" in turns and i < len(turns["dialogue_acts"]):
+                    act = turns["dialogue_acts"][i]
+                    span_info = act.get("span_info", {})
+
+                    for act_slot_name, act_slot_value, span_start, span_end in zip(
+                        span_info.get("act_slot_name", []),
+                        span_info.get("act_slot_value", []),
+                        span_info.get("span_start", []),
+                        span_info.get("span_end", []),
+                    ):
+                        # Finding the actual span in the utterance
+                        actual_span = utterance[span_start:span_end]
+
+                        # Storing unique slot value and span tuples
+                        slot_span_tuple = (act_slot_name, act_slot_value, actual_span)
+                        if slot_span_tuple not in extracted_data:
+                            extracted_data.append(slot_span_tuple)
+
+        return extracted_data
+
+    def get_mismatched_slot_values(self, extracted_data):
+        """
+        Get the mismatched slot values stored as a dataframe
+        :param extracted_data: Extracted slot spans from the dataset in the format (slot_name, slot_value, actual_span).
+        """
+
+        mismatched_slot_values = []
+        for slot_name, slot_value, actual_span in extracted_data:
+            if slot_value != actual_span:
+                mismatched_slot_values.append([slot_name, slot_value, actual_span])
+        mismatched_slot_values = pd.DataFrame(
+            mismatched_slot_values, columns=["slot_name", "slot_value", "actual_span"]
+        )
+        return mismatched_slot_values
+
+    def convert_span_to_slot_value(
+        self, slot_name, actual_span, dialogue_slot_memory=None
+    ):
+        """
+        Convert the actual span to the slot value
+        :param slot_name: Name of the slot.
+        :param actual_span: Actual span in the utterance.
+        :param dialogue_slot_memory: Dialogue slot memory object containing the slot values from the dialogue history.
+        """
+
+        # Remove the lowercase and whitespace initially
+        standardized_span = actual_span.lower().strip()
+
+        # dictionary to convert strings to number representations
+        number_dict = {
+            "one": "1",
+            "two": "2",
+            "three": "3",
+            "four": "4",
+            "five": "5",
+            "six": "6",
+            "seven": "7",
+            "eight": "8",
+            "nine": "9",
+            "ten": "10",
+            "eleven": "11",
+            "twelve": "12",
+            "thirteen": "13",
+            "fourteen": "14",
+            "fifteen": "15",
+            "sixteen": "16",
+            "seventeen": "17",
+            "eighteen": "18",
+            "nineteen": "19",
+            "twenty": "20",
+        }
+
+        # if span is a number word, convert it to a number and return
+        if standardized_span in number_dict:
+            return number_dict[standardized_span]
+
+        # if the slot is a postcode, uppercase the letters and return
+        if slot_name == "postcode":
+            return standardized_span.upper()
+
+        # arbitrary spelling for "centre" in the dataset
+        if standardized_span == "center":
+            return "centre"
+
+        # if slot is a time, convert the first word to a number and uppercase the second word
+        if slot_name == "booktime":
+            # check if the span consists of two words
+            if len(standardized_span.split()) >= 2:
+                # convert the first word to a number
+                partial = number_dict.get(
+                    standardized_span.split()[0], standardized_span.split()[0]
+                )
+                # uppercase the second word
+                standardized_span = partial + " " + standardized_span.split()[1].upper()
+                return standardized_span
+            else:
+                return standardized_span
+
+        # if span indicates indifference to a slot value, return "dontcare"
+        if standardized_span in [
+            "any",
+            "anything",
+            "anywhere",
+            "dont care",
+            "dontcare",
+            "dontcare",
+            "don't care",
+        ]:
+            return "dontcare"
+
+        # if span contains 'same' return the relevant value from dialogue history
+        if "same" in standardized_span.split() and dialogue_slot_memory is not None:
+            # get the most recent slot value from the dialogue history
+            return dialogue_slot_memory.get_most_recent_slot_value(slot_name)
+
+        return standardized_span
+
 
 def train_and_test_model():
     # Create the dataset
@@ -594,8 +756,8 @@ def train_and_test_model():
     res = model.query("I want to book a table for 4 people at 7pm tonight.")
     print(res)
 
-if __name__ == "__main__":
-    
+
+def load_and_query_model(query, model_path="checkpoint_epoch_3.pt"):
     # Create dialogues dataset'
     dataset = SlotFillingDataset()
     # Load the dataset
@@ -625,22 +787,166 @@ if __name__ == "__main__":
     val_dataloader = tensor_dataset_val.create_dataloader()
     # Create the dataloader for test
     test_dataloader = tensor_dataset_test.create_dataloader()
-    # Create the model
-    dialogue_data = dataset.create_labelled_dialogue_data(dataset.test_dataset)
     # create model from checkpoint
     model = SlotFillingModel(
-    train_dataloader,
-    val_dataloader,
-    test_dataloader,
-    dataset.tokenizer,
-    num_labels,
-    label2id,
-    dataset.id2label(label2id),
-)
+        train_dataloader,
+        val_dataloader,
+        test_dataloader,
+        dataset.tokenizer,
+        num_labels,
+        label2id,
+        dataset.id2label(label2id),
+    )
     model.create_model()
-    model.model.load_state_dict(torch.load("checkpoint_epoch_3.pt"))
+    model.model.load_state_dict(torch.load(model_path))
     model.model.eval()
     # Query the model
-    res = model.query_slots("I want to book a table for 4 people at 7pm tonight.")
+    res = model.query_slots(query)
     print(res)
-    
+
+
+def test_span_to_slot_value_mapping():
+    # Create the dataset
+    dataset = SlotFillingDataset()
+    # Load the dataset
+    dataset.load()
+    # Filter the dataset
+    dataset.get_relevant_data({"restaurant", "hotel"})
+    # Create the labelled training data
+    labelled_data = dataset.create_labelled_data(dataset.train_dataset)
+    # Create the labelled validation data
+    labelled_data_val = dataset.create_labelled_data(dataset.val_dataset)
+    # Create the labelled test data
+    labelled_data_test = dataset.create_labelled_data(dataset.test_dataset)
+    # Create the label2id mapping
+    label2id, num_labels = dataset.create_label2id(labelled_data)
+    # get the slot spans and values
+    parser = SlotValueParser()
+    # Get the unique slots
+    slot_spans = parser.extract_slot_spans(dataset.train_dataset)
+    # Get the mismatched slot values
+    mismatched_slot_values = parser.get_mismatched_slot_values(
+        slot_spans
+    )  # get the mismatched slot values
+
+    fix_count = 0
+    total_mismatched_slot_values = len(mismatched_slot_values)
+
+    for slot_name, slot_value, actual_span in mismatched_slot_values.values:
+        # convert the actual span to the slot value
+        fixed_span = parser.convert_span_to_slot_value(slot_name, actual_span)
+        if slot_value == fixed_span:
+            print(
+                f"Corrected slot value for '{slot_name}': '{actual_span}' -> '{fixed_span}'"
+            )
+            fix_count += 1
+    print(f"Fixed {fix_count}/{total_mismatched_slot_values} slot values.")
+
+
+def test_full_dialogue_system(
+    path_to_model_checkpoint="checkpoint_epoch_3.pt", print_output=False
+):
+    # Create dialogues dataset'
+    dataset = SlotFillingDataset()
+    # Load the dataset
+    dataset.load()
+    # Filter the dataset
+    dataset.get_relevant_data({"restaurant", "hotel"})
+    # Create the labelled training data
+    labelled_data = dataset.create_labelled_data(dataset.train_dataset)
+    # Create the labelled validation data
+    labelled_data_val = dataset.create_labelled_data(dataset.val_dataset)
+    # Create the labelled test data
+    labelled_data_test = dataset.create_labelled_data(dataset.test_dataset)
+    # Create the label2id mapping
+    label2id, num_labels = dataset.create_label2id(labelled_data)
+    # Create the TensorDataset for training
+    tensor_dataset_train = TensorDataset(labelled_data, label2id)
+    tensor_dataset_train.create_tensors()
+    # Create the TensorDataset for validation
+    tensor_dataset_val = TensorDataset(labelled_data_val, label2id)
+    tensor_dataset_val.create_tensors()
+    # Create the TensorDataset for test
+    tensor_dataset_test = TensorDataset(labelled_data_test, label2id)
+    tensor_dataset_test.create_tensors()
+    # Create the dataloader for training
+    train_dataloader = tensor_dataset_train.create_dataloader()
+    # Create the dataloader for validation
+    val_dataloader = tensor_dataset_val.create_dataloader()
+    # Create the dataloader for test
+    test_dataloader = tensor_dataset_test.create_dataloader()
+    # create model from checkpoint
+    model = SlotFillingModel(
+        train_dataloader,
+        val_dataloader,
+        test_dataloader,
+        dataset.tokenizer,
+        num_labels,
+        label2id,
+        dataset.id2label(label2id),
+    )
+    model.create_model()
+    model.model.load_state_dict(torch.load(path_to_model_checkpoint))
+    model.model.eval()
+
+    # create labeled dialogue data
+    labelled_dialogue_data = dataset.create_labelled_dialogue_data(dataset.test_dataset)
+
+    # query the model for each dialogue
+
+    num_slots_correct = 0
+    num_slots_total = 0
+
+    num_labels_correct = 0
+    num_labels_total = 0
+
+    num_slots_filled_correct = 0
+    num_slots_filled_total = 0
+
+    for dialogue in labelled_dialogue_data:
+        dialogue_slot_memory = DialogSlotMemory()
+        parser = SlotValueParser()
+        for utterance, gt_slot_values in dialogue:
+            predicted_slots = model.query_slots(utterance)
+
+            for slot_name, predicted_value in predicted_slots.items():
+                # Convert the predicted slot value
+                predicted_value = parser.convert_span_to_slot_value(
+                    slot_name, predicted_value, dialogue_slot_memory
+                )
+
+                # Update the predicted slots dictionary with the converted value
+                predicted_slots[slot_name] = predicted_value
+
+                # Update dialogue slot memory
+                dialogue_slot_memory.add_slot(slot_name, predicted_value)
+
+            # Compare the dictionaries
+            num_slots_total += len(gt_slot_values)
+            num_slots_filled_total += len(predicted_slots)
+
+            for slot_name, gt_value in gt_slot_values.items():
+                if slot_name in predicted_slots:
+                    num_slots_correct += 1
+                    if predicted_slots[slot_name] == gt_value:
+                        num_slots_filled_correct += 1
+                    num_labels_total += 1
+                    if gt_value == predicted_slots[slot_name]:
+                        num_labels_correct += 1
+
+            if print_output:
+                print(f"Utterance: '{utterance}'")
+                print(f"Predicted Slots: {predicted_slots}")
+                print(f"Ground Truth Slots: {gt_slot_values}")
+                print("-" * 50)
+
+    # Number of slots that were identified correctly
+    print(f"Slot Accuracy: {num_slots_correct/num_slots_total}")
+    # Fraction of values that were parsed correctly
+    print(f"Value Accuracy: {num_labels_correct/num_labels_total}")
+    # Fraction of slots that were filled correctly
+    print(f"Slot/Value Accuracy: {num_slots_filled_correct/num_slots_filled_total}")
+
+
+if __name__ == "__main__":
+    test_full_dialogue_system()
